bd528d43a50d1ff0b8c2cf4d18e28da30f110a09
Gautham Anne <rnl2121@joker.ece.northwestern.edu>
2026-03-12 18:22:01 -0500

    fir inits

---
+++ sv/fir_tb.sv
@@ -0,0 +1,10 @@
+`timescale 1 ns / 1 ns
+
+module fir_tb;
+localparam string IMG_IN_NAME  = "image.bmp";
+localparam string IMG_OUT_NAME = "output.bmp";
+localparam string IMG_CMP_NAME = "grayscale.bmp";
+localparam CLOCK_PERIOD = 10;
+
+
+endmodule
